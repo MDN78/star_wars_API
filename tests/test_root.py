089944952426c5e4437d1f0b909cc3a1023c95d2
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-10-27 20:14:49 +0300

    update tests

--- tests/test_root.py
+++ tests/test_root.py
@@ -11,9 +11,12 @@ class TestRoot:
         @allure.title('Gett all roots')
         def test_get_all_roots(self, api_session):
             response = api_session.request(method='GET', path='/')
-            assert response.status_code == 200
-            assert response.headers.get('content-type') == 'application/json'
-            root = Root.model_validate(response.json())
+            with allure.step('Status code = 200'):
+                assert response.status_code == 200
+            with allure.step('Check content type'):
+                assert response.headers.get('content-type') == 'application/json'
+            with allure.step('Validate response json'):
+                Root.model_validate(response.json())
 
     @allure.story('Negative tests')
     class TestNegative:
@@ -21,5 +24,7 @@ class TestRoot:
         @allure.title('Get bad request')
         def test_404(self, api_session):
             response = api_session.request(method='GET', path='/wrong/')
-            assert response.status_code == 404
-            assert response.headers.get('Content-Type') == 'text/html'
+            with allure.step('Status code = 404'):
+                assert response.status_code == 404
+            with allure.step('Response headers'):
+                assert response.headers.get('Content-Type') == 'text/html'
